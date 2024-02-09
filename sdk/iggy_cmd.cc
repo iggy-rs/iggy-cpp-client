2d1edf06aff289a97c3e3ed257e368954dc07685
0xG0nz0 <8682922+0xg0nz0@users.noreply.github.com>
2024-02-09 00:17:08 +0000

    Interim commit for sample CLI usage

--- sdk/iggy_cmd.cc
+++ sdk/iggy_cmd.cc
@@ -1,11 +1,24 @@
+#define FMT_HEADER_ONLY
+
 #include <iostream>
 #include "argh.h"
+#include "cli/cli.h"
+#include "spdlog/spdlog.h"
 
 int main(int, char* argv[]) {
     argh::parser cmdl(argv);
 
-    if (cmdl[{"-v", "--verbose"}]) {
-        std::cout << "Verbose, I am.\n";
+    if (cmdl[iggy::cli::VERBOSE_FLAGS]) {
+        spdlog::set_level(spdlog::level::debug);
     }
+
+    if (cmdl[iggy::cli::HELP_FLAGS] || (cmdl.pos_args().size() != 2)) {
+        iggy::cli::print_usage(cmdl[0]);
+        return EXIT_SUCCESS;
+    }
+
+    spdlog::debug("Iggy C++ CLI");
+    spdlog::debug(cmdl.pos_args()[1]);
+
     return EXIT_SUCCESS;
 }
