c84a887cbf52085d71fb81f1dd96df399ed0b664
0xG0nz0 <8682922+0xg0nz0@users.noreply.github.com>
2024-03-09 17:13:36 +0000

    Set up pre-commit; clean up configurations

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+script_dir="$(dirname -- "$(realpath -- "$0")")"
+workspace_dir="$(dirname "${script_dir}")"
 
-# install extra local linting tools
-pip install cpplint
+# fix Git
+if ! git config --global --get safe.directory > /dev/null; then
+    git config --global --add safe.directory "${workspace_dir}"
+fi
+
+# set up pre-commit
+pre-commit install
