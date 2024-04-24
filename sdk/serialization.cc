3959ec75ddacb07deb56d33173d7730713f60d87
0xG0nz0 <8682922+0xg0nz0@users.noreply.github.com>
2024-04-24 17:54:31 +0000

    Convert from C++17 to C++20

--- sdk/serialization.cc
+++ sdk/serialization.cc
@@ -4,16 +4,17 @@
 #include <stdexcept>
 
 std::string icp::serialization::convertToUTF8(const std::string& source, bool strict) {
-    if (utf8valid(source.c_str()) == 0) {
+    std::unique_ptr<char8_t[]> data(new char8_t[source.size() + 1]);
+    std::copy(source.begin(), source.end(), data.get());
+    data[source.size()] = u8'\0';
+    if (utf8valid(data.get()) == 0) {
         return source;
     } else {
         if (strict) {
             throw std::invalid_argument(fmt::format("The input string is not a valid UTF-8 string: '{}'", source));
         } else {
-            char* data = new char[source.size() + 1];
-            std::snprintf(data, source.size() + 1, "%s", source.c_str());
-            utf8makevalid(data, '?');
-            return std::string(data);
+            utf8makevalid(data.get(), '?');
+            return std::string(reinterpret_cast<char*>(data.get()));
         }
     }
 }
